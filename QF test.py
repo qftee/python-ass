2f4fa2fa4f159e19e0a678e3934f81ca271c6af9
qftee <qftee1023@gmail.com>
2023-09-20 19:36:46 +0800

    open txt file

---
+++ QF test.py
@@ -0,0 +1,53 @@
+def create_suppliers():
+    jqsb = f'JQSB, JQ Sdn.Bhd, MS,FS, date'
+    qfsb = f'QFSB, QF Sdn Bhd, GL,HC, date'
+    pysb = f'PYSB, PY Sdn Bhd, GW,SC, date'
+    with open('suppliers.txt', 'w') as create_supplier:
+        create_supplier.write(jqsb + '\n')
+        create_supplier.write(qfsb + '\n')
+        create_supplier.write(pysb + '\n')
+
+
+try:
+    with open('suppliers.txt','r') as supplier:
+        check_suppliers_available = supplier.read()
+        if check_suppliers_available == '':
+            create_suppliers()
+except FileNotFoundError:
+    create_suppliers()
+
+
+def create_hospitals():
+    sphs = f'SPHS, Sungai Pal Hospital, MS,FS, date'
+    mwhs = f'MWHS, Mou Wish Hospital, GL,HC, date'
+    cshs = f'CSHS, Cyber Science Hospital, GW,SC, date'
+    with open('hospital.txt', 'w') as create_hospital:
+        create_hospital.write(sphs + '\n')
+        create_hospital.write(mwhs + '\n')
+        create_hospital.write(cshs + '\n')
+
+
+try:
+    with open('hospital.txt', 'r') as hospital:
+        check_hospital_available = hospital.read()
+        if check_hospital_available == '':
+            create_hospitals()
+except FileNotFoundError:
+    create_hospitals()
+
+
+def create_items():
+    hc = f'HC, QFSB, 100, date, date'
+    fs = f'FS, JQSB, 100, date, date'
+    ms = f'MS, JQSB, 100, date, date'
+    gl = f'GL, QFSB, 100, date, date'
+    gw = f'GW, PYSB, 100, date, date'
+    sc = f'SC, PYSB, 100, date, date'
+    with open('ppe.txt', 'w') as create_item:
+        create_item.write(hc + '\n')
+        create_item.write(fs + '\n')
+        create_item.write(ms + '\n')
+        create_item.write(gl + '\n')
+        create_item.write(gw + '\n')
+        create_item.write(sc + '\n')
+
--- test.py
+++
@@ -1,4 +0,0 @@
-def create_suppliers():
-    with open('suppliers.txt','w') as create_supplier:
-        create_supplier.write['JQSB','JQ Sdn.Bhd',]
-
