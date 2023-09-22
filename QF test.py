d635714d764013ea2496ebd6470fdd36315b5ae3
qftee <qftee1023@gmail.com>
2023-09-23 00:47:44 +0800

    Improve the update invetory system

--- QF test.py
+++ QF test.py
@@ -1,3 +1,15 @@
+import datetime
+
+
+def main_menu():
+    pass
+
+
+def date_time():
+    now = datetime.datetime.now()
+    return now.strftime('%c')
+
+
 def create_suppliers():
     jqsb = f'JQSB, JQ Sdn.Bhd, MS,FS, date'
     qfsb = f'QFSB, QF Sdn Bhd, GL,HC, date'
@@ -9,7 +21,7 @@ def create_suppliers():
 
 
 try:
-    with open('suppliers.txt','r') as supplier:
+    with open('suppliers.txt', 'r') as supplier:
         check_suppliers_available = supplier.read()
         if check_suppliers_available == '':
             create_suppliers()
@@ -51,28 +63,66 @@ def create_items():
         create_item.write(gw + '\n')
         create_item.write(sc + '\n')
 
-create_items()
-def supply():
-    supply_item = input('Item code:')
-    quantity = int(input('Quantity(Boxes):'))
-    supply_item.upper()
-
-
 
 def update_inventory():
-    supply_or_distribute = int(input('''"1" Supply
-    "2" Distribute
-    "3" Exit
-    >'''))
-    if supply_or_distribute == 1:
-        supply
+    how_to_manage = int(input('''"1" Supply
+"2" Distribute
+"3" Main Menu
+>'''))
+    if how_to_manage == 1:
+        supply_or_distribute('supply')
 
-    elif supply_or_distribute == 2:
-        distribute
+    elif how_to_manage == 2:
+        supply_or_distribute('distribute')
 
-    elif supply_or_distribute == 3:
-        mainmenu
+    elif how_to_manage == 3:
+        main_menu()
 
     else:
         print("Sorry, I don't understand")
-        update_inventory
+        update_inventory()
+
+
+def supply_or_distribute(s_or_d):
+    supply_item = str(input('Item code:'))
+    quantity_changes = int(input('Quantity(Boxes):'))
+    supply_item = supply_item.upper().strip()
+    with open('ppe.txt', 'r') as edit_quantity:
+        lines = edit_quantity.readlines()
+
+    with open('ppe.txt', 'w') as edit_quantity:
+        for item in lines:
+            if item.startswith(supply_item):
+                item_info = item.split(',')
+                item_code = item_info[0]
+                item_name = item_info[1]
+                item_quantity = int(item_info[2])
+                last_supply_date = item_info[3]
+                last_distribute_date = item_info[4]
+                if s_or_d == 'supply':
+                    check_again = input(f'The remaining quantity is "{item_quantity}" boxes,\n'
+                                        f'Are you sure want to add it ? \nYes(Y)  No(N)\n>')
+                    if check_again.upper() == 'Y':
+                        item_info[2] = str(item_quantity + quantity_changes)
+                        item_info[3] = date_time()
+                        print('Item successfully updated')
+                    elif check_again.upper() == 'N':
+                        print('Item Update Failed')
+
+                    else:
+                        print("Sorry I Don't Understand , Please type 'Y' or 'N'")
+                elif s_or_d == 'distribute':
+                    check_again = input(f'The remaining quantity is "{item_quantity}" boxes,\n'
+                                        f'Are you sure want to distribute ? \nYes(Y)  No(N)\n>')
+                    if check_again.upper() == 'Y':
+                        item_info[2] = str(item_quantity - quantity_changes)
+                        item_info[4] = date_time()
+                        print('Item successfully updated')
+                    else:
+                        print('Item Update Failed')
+                edit_quantity.write(','.join(item_info))
+            else:
+                edit_quantity.write(item)
+
+
+update_inventory()
